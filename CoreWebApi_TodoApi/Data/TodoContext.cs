a776624ea7056adbba8c7e1339b5a3b0b0927898
wanchun <chuang630cm40630@gamil.com>
2020-10-05 19:38:05 +0800

    加入專案檔案。

---
+++ CoreWebApi_TodoApi/Data/TodoContext.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CoreWebApi_TodoApi.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CoreWebApi_TodoApi.Data
+{
+    public class TodoItemContext : DbContext
+    {
+        public TodoItemContext(DbContextOptions<TodoItemContext> options) : base(options)
+        {
+        }
+
+        public DbSet<TodoItem> TodoItems { get; set; }
+    }
+}
